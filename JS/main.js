d93a6ba4e3a8390c453b82849d0afb12a3047452
Kayke Ragoso <162835740+KaykeRagoso@users.noreply.github.com>
2026-01-14 11:21:44 -0300

    Feito o FormData para enviar texto ou arquivo

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,45 @@
 const form = document.getElementById("email-form");
-cosnt emailText = document.getElementById("emailText");
+const emailText = document.getElementById("emailText");
 const emailFile = document.getElementById("emailFile");
+const loading = document.getElementById("loading");
+const resultSection = document.getElementById("result");
+const categorySpan = document.getElementById("category");
+const replyParagraph = document.getElementById("reply");
+const resetBtn = document.getElementById("resetBtn");
 
 form.addEventListener("submit", async (e) => {
     event.preventDefault();
 
     //Validar
-    if(!emailText.value && !emailFile.isDefaultNamespace.length){
+    if(!emailText.value && !emailFile.files.length){
         alert("Por favor, insira o texto do email ou selecione o arquivo.")
         return;
+    }
+
+    loading.classList.remove("hidden");
+    resultSection.classList.add("hidden");  
+
+    const formData = new FormData();
+    formData.append("emailText",emailText.value);
+
+    if (emailFile.files.length > 0) {
+        formData.append("emailFile",emailFile.files[0]);
+    }
+    try {
+        const resposta = await fetch("/analyze", {
+            method: "POST",
+            body: formData,
+    });
+        if(!resposta.ok){
+            throw new Error("Erro ao processar o email.");
+        }
+        const data = await resposta.json();
+
+        showResult(data.category, data.reply);
+    } catch (error) {
+        alert("Ocorreu um erro ao processar o email. Tente novamente.");
+        console.error("Erro:", error);
+    } finally{
+        loading.classList.add("hidden");
     }
 }); 
